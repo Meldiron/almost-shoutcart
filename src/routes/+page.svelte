772d42ad3e46aec63450e3545fb850e97a82080d
Matej Baco <matejbacocom@gmail.com>
2022-09-17 17:50:10 +0200

    Added order history layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,29 @@
 	import { AppwriteService } from '$lib/AppwriteService';
 
 	let msg = '';
+
+	const orders = [
+		{
+			$createdAt: '2022-09-17T14:10:21.090+00:00',
+			msg: 'Hello World!',
+			status: 'FINISHED'
+		},
+		{
+			$createdAt: '2022-09-17T14:10:21.090+00:00',
+			msg: 'Hello World!',
+			status: 'PENDING'
+		},
+		{
+			$createdAt: '2022-09-17T14:10:21.090+00:00',
+			msg: 'Hello World!',
+			status: 'CREATED'
+		},
+		{
+			$createdAt: '2022-09-17T14:10:21.090+00:00',
+			msg: 'Hello World!',
+			status: 'CANCELLED'
+		}
+	];
 </script>
 
 <h1>Homepage</h1>
@@ -26,7 +49,7 @@
 	</p>
 
 	<form on:submit|preventDefault={() => AppwriteService.createOrder(msg)}>
-		<div class="form-group">
+		<div class="form-group mb-2">
 			<label for="yourMessage">Your message</label>
 			<textarea
 				bind:value={msg}
@@ -39,8 +62,24 @@
 		</div>
 		<button type="submit" class="btn btn-primary">Buy for 0.99€</button>
 
-		<div>
+		<div class="mt-2">
 			<small>Payment is done in a sandbox environment. You don't pay anything, no worries.</small>
 		</div>
 	</form>
+
+	<hr />
+
+	<h3>Order History</h3>
+
+	{#if orders.length <= 0}
+		<p>No orders in your history.</p>
+	{/if}
+
+	<ul>
+		{#each orders as order}
+			<li><b>[{order.status}]</b> {order.msg} <small>({order.$createdAt})</small></li>
+		{/each}
+	</ul>
+
+	<button class="btn btn-secondary">Load more</button>
 {/if}
