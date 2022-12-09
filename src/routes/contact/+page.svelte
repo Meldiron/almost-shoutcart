3e8b24d0667858415920f7c7821a050f4601b27d
Matej Bačo <matejbaco2000@gmail.com>
2022-12-09 21:21:07 +0100

    Improve contact

--- src/routes/contact/+page.svelte
+++
@@ -1,4 +0,0 @@
-<div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-    <h1 class="display-4 fw-normal">Wanna Get in Touch?</h1>
-    <p class="fs-5 text-muted">In case of any questions feel free to reach to me, a developer of this website, at <span class="text-primary">matej@appwrite.io</span>.</p>
-</div>
